6f8e9d1e85c83410eb127eb59e20ea3d6496e0b7
ayafounes <founesaya19@gmail.com>
2024-10-18 21:42:12 +0100

    evenements

--- src/app/recherche-par-nom/recherche-par-nom.component.ts
+++ src/app/recherche-par-nom/recherche-par-nom.component.ts
@@ -5,27 +5,25 @@ import { EvenementService } from '../services/evenement.service';
 @Component({
   selector: 'app-recherche-par-nom',
   templateUrl: './recherche-par-nom.component.html',
-  styles: ``
+  styles: []
 })
 export class RechercheParNomComponent {
-  nomEvenement!: string;
+ 
   evenements!: evenement[];
-  allEvenements!: evenement[];
-  searchTerm!: string;
+  allEvenements!: evenement[]; // Holds the original list of events
+  searchTerm!: string ;
 
-  constructor(private ett: EvenementService){
+  constructor(private ett: EvenementService) {}
+
+  ngOnInit(): void {
+    this.allEvenements = this.ett.ListeEvenements(); // Initialize allEvenements
+    
   }
 
-  ngOnInit():void{
-    this.evenements=this.ett.ListeEvenements();
-  }  
-
-  
-    
-
-  onKeyUp(filterText : string){
+  onKeyUp(filterText: string): void {
+    // Filter the original array to update the displayed list of events
     this.evenements = this.allEvenements.filter(item =>
-    item.nomEvenement.toLowerCase().includes(filterText));
-    }
-    
+      item.nomEvenement.toLowerCase().includes(filterText.toLowerCase()) // Ensure case insensitivity
+    );
+  }
 }
